126570e5b2f257313caef38d069eeb096891ad14
amrit duggal <61247153+klvnpaul@users.noreply.github.com>
2023-02-02 19:00:50 +0300

    Update sysinfo.sh
    lab 2

--- sysinfo.sh
+++ sysinfo.sh
@@ -3,55 +3,74 @@
 # Script to display  important identy information about a computer
 
 # Get FQDN  
-echo " " 
+#echo " " 
 
-echo "INFORMATION ABOUT THE IDENTITY OF THE COMPUTER"
+#echo "INFORMATION ABOUT THE IDENTITY OF THE COMPUTER"
 
-echo " "
+#echo " "
 
 fqdn=$(hostname -f)
 
-echo "The fully-qualified DOmain Name: $fqdn" 
+#echo "The fully-qualified DOmain Name: $fqdn" 
 
 # Get the Operating System name and version 
 
 operating_system=$(hostnamectl | grep "Operating System")
 
-echo "$operating_system" 
+#echo "$operating_system" 
 
 # Get non-127 IP Adresses 
 
 ip_addresses=$(hostname -I | tr ' ' '\n' | grep -v "^127.")
 
-echo "IP Addresses: $ip_addresses" 
+#echo "IP Addresses: $ip_addresses" 
 
 # Get available space in root filesystem
 root_space=$(df -h / | tail -l | awk '{print $4}')
-echo "Available space in the root filesystem is: $root_space"
+#echo "Available space in the root filesystem is: $root_space"
 #echo "Available space in the root filesystem: $root_space "
 
 #Get the boot id
 
 boot_id=$(cat /proc/sys/kernel/random/boot_id)
-echo "THe Boot ID is: $boot_id"
+#echo "THe Boot ID is: $boot_id"
 
 #Get the chassis
 chasis=$(cat /sys/class/dmi/id/chassis_vendor)
-echo "Chassis: $chasis"
+#echo "Chassis: $chasis"
 
 #Get the Kernel
 kernel=$(uname -r)
-echo "THe kernel is : $kernel"
+#echo "THe kernel is : $kernel"
 
 #Get architechture of the computer
-
 architecture=$(uname -m)
-echo "Achitecture : $architecture"
+#echo "Achitecture : $architecture"
 
 #Get root filesystem status 
 root_filesystem=$(df -T / | awk '{if ($1 == "Filesystem") print $2}')
 
-echo "Root filesystem status: $root_filesystem"
+#echo "Root filesystem status: $root_filesystem"
 
-echo " "
+#echo " "
 
+#output template
+
+cat <<EOF
+
+INFORMATION ABOUT THE IDENTITY OF THE COMPUTER
+
+-----------------------------------------------
+
+The fully-qualified DOmain Name: $fqdn
+Operating system: $operating_system
+IP Addresses: $ip_addresses
+Available space in the root filesystem is: $root_space
+THe Boot ID is: $boot_id
+Chassis: $chasis
+THe kernel is : $kernel
+Achitecture : $architecture
+Root filesystem status: $root_filesystem
+
+-----------------------------------------------
+EOF
